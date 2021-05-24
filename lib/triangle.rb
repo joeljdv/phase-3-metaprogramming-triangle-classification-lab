a54c8eda2c2822f7a4c41b50fc0afe81686f45a0
joeljdv <joeldiazjdv@gmail.com>
2021-05-24 14:40:14 -0400

    commit tree 75a6b8e1a2128940fc309b26c59e930137c7b345

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,30 @@
 class Triangle
   # write code here
+  attr_accessor :a, :b, :c
+  def initialize(a, b ,c)
+    @a = a 
+    @b = b
+    @c = c
+  end
+
+  def kind
+    if @a == @b && @b == @c
+      :equilateral
+    elsif @a == @b && @b != @c || @a == @c && @c != @b || @b == @c && @c != @a
+      :isosceles
+    elsif @a != @b && @b != @c
+      :scalene
+    elsif @a = 0 && @b = 0 && @c == 0
+      raise TriangleError
+    end
+  end
+
+  class TriangleError < StandardError
+
+    def message
+      "You have an error"
+    end
+
+  end
+
 end
